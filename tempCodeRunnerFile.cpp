8341f95be1c061e53f0944f7c7819a2c461d688f
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-07 12:19:52 +0700

    memasukan element array

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+  cout << "\n=========================\n";
+    cout << "   Masukkan Elemen Array   \n";
+    cout << "\n=========================\n";
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + i) << " = ";
+        cin >> element [i];
+    }
+}
