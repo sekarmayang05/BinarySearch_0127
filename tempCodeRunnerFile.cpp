3cbd887d73436f93421cdda12caa8e43ade72afd
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-05-07 13:03:50 +0700

    mencari ulang

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,5 +1,6 @@
- }
-            if (x > element[mid])
-            {
-                low = mid +1;
-            }
+ {
+          cout << "\n[✘] Elemen "<< x <<" tidak ditemukan dalam array.\n"; 
+        }
+        cout << "\nIngin mencari lagi (y\n): ";
+        cin >> ulang;
+    } while (ulang == 'y' || ulang == 'Y');
